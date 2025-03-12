11054655b9e62b044cf3a4520fa1d38ac16f3359
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-12 19:25:50 +0300

    Update SharedWallet.sol

--- Wallet/SharedWallet.sol
+++ Wallet/SharedWallet.sol
@@ -1,29 +1,35 @@
-//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SharedWallet is Ownable {
-  mapping(address => uint) public members;
-    
-  function isOwner() internal view returns(bool) {
-    return owner() == _msgSender();
-  }
-    
-  modifier ownerOrWithinLimits(uint _amount) {
-    require(isOwner() || members[_msgSender()] >= _amount, "You are not allowed to perform this operation!");
-    _;
-  }
-    
-  function addLimit(address _member, uint _limit) public onlyOwner {
-    members[_member] = _limit;     
-  }
-    
-  function deduceFromLimit(address _member, uint _amount) internal {
-    members[_member] -= _amount;
-  }
-    
-  function renounceOwnership() override public view onlyOwner {
-    revert("Can't renounce!");
-  }
+    // Mapping of user addresses to their withdrawal limits
+    mapping(address => uint) public members;
+
+    // Function to add a user with a specified limit
+    function addLimit(address _member, uint _limit) public onlyOwner {
+        members[_member] = _limit;
+    }
+
+    // Internal function to check if the sender is the owner
+    function isOwner() internal view returns(bool) {
+        return owner() == _msgSender();
+    }
+
+    // Modifier to ensure the sender is either the owner or within their limit
+    modifier ownerOrWithinLimits(uint _amount) {
+        require(isOwner() || members[_msgSender()] >= _amount, "You are not allowed to perform this operation!");
+        _;
+    }
+
+    // Function to deduct from a user's limit
+    function deduceFromLimit(address _member, uint _amount) internal {
+        members[_member] -= _amount;
+    }
+
+    // Override renounceOwnership to prevent it from being called
+    function renounceOwnership() override public view onlyOwner {
+        revert("Can't renounce!");
+    }
 }
