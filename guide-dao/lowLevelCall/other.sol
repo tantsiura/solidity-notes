6f9727840e7f93b9a788fc1870539add85980cdf
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-14 23:09:07 +0300

    Update other.sol

--- guide-dao/lowLevelCall/other.sol
+++ guide-dao/lowLevelCall/other.sol
@@ -4,52 +4,38 @@ pragma solidity ^0.8.0;
 
 contract Other {
     bytes public returnedData;
-    
-    function callGetName(address _demo) external {
-        name = _newName;
-        age = _newAge;
-    }
+    string public returnedName;
 
-    function getName() public view returns(string memory) {
+    function callGetName(address _demo) external {
         (bool result, bytes memory data) = _demo.call(
             abi.encodeWithSignature(
                 "getName()"
             )
-        )
+        );
         require(result, "failed");
 
         returnedData = data;
-        returnedName = string()ability.encodePacked(data));
+        returnedName = abi.decode(data, (string));
     }
 
-    function callSetData(address _demo, string calldata _newName) {
+    function callSetData(address _demo, string calldata _newName, uint _newAge) external {
         (bool result) = _demo.call(
             abi.encodeWithSignature(
                 "setData(string,uint256)",
                 _newName,
                 _newAge
             )
-        )
+        );
         require(result, "failed");
     }
 
-    function callPay(address _demo) external paybale {
+    function callPay(address _demo) external payable {
         (bool result,) = _demo.call{value: msg.value} (
             abi.encodeWithSignature(
                 "pay(address)",
                 msg.sender
             )
-        )
-        require(result, "failed");
-    }
-
-    function callNonexsistent(address _demo) external paybale {
-        (bool result,) = _demo.call(
-            abi.encodeWithSignature(
-                "fake",
-                msg.sender
-            )
-        )
+        );
         require(result, "failed");
     }
 }
