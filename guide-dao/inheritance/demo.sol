aee1da1f0c691669a91de7ee5f7680e0b92ac6a1
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-15 11:54:58 +0300

    Update demo.sol

--- guide-dao/inheritance/demo.sol
+++ guide-dao/inheritance/demo.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import "./Owned.sol";
 
 contract Demo is Owned {
-
     function withdraw(address payable _to) public onlyOwner {
-        addres thisContacrt = address(this);
-        _to.transfer(thisContacrt.balance);
+        address thisContract = address(this);
+        (bool sent, ) = _to.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
     }
 
     function parentFunc() public override pure returns(uint) {
