b1c4d928f8aec6c6a90d5b61f8ab86222805632a
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-15 11:50:15 +0300

    Update demo.sol

--- guide-dao/inheritance/demo.sol
+++ guide-dao/inheritance/demo.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+import "./Owned.sol";
+
+contract Demo is Owned {
+
+    function withdraw(address payable _to) public onlyOwner {
+        addres thisContacrt = address(this);
+        _to.transfer(thisContacrt.balance);
+    }
+
+    function parentFunc() public override pure returns(uint) {
+        return 100;
+    }
+}
