dfcf6eab994c928f568fc2eb26bf0978173860c4
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-15 11:53:42 +0300

    Update owned.sol

--- guide-dao/inheritance/owned.sol
+++ guide-dao/inheritance/owned.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+
+contract Owned {
+
+    constructor {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Not an owner!");
+        _;
+    }
+
+    function parentFunc() public virtual pure returns(uint) {
+        return 42;
+    }
+}
