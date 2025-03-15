24d215d16ac14e47d2348f85cc9628aef805b476
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-15 10:59:04 +0300

    Update demo.sol

--- guide-dao/DelegatedCall/demo.sol
+++ guide-dao/DelegatedCall/demo.sol
@@ -1,1 +1,16 @@
 
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Demo {
+    address public sender;
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function pay() external payable {
+        sender = msg.sender;
+    }
+}
