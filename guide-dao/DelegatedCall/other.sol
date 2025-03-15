7cddb4e20334f1d29e4e8af3a478f322ef3b4f82
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-15 10:57:17 +0300

    Update other.sol

--- guide-dao/DelegatedCall/other.sol
+++ guide-dao/DelegatedCall/other.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+interface IDemo {
+    function pay() external payable;
+}
+
+contract Other {
+    address public sender;
+    uint public amount;
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function callPay(address _demo) external payable {
+        (bool result) = _demo.delegetecall(
+            // abi.encodeWithSignature(
+            //     "pay(string)"
+            // )
+            abi.encodeWithSelector(
+                IDemo.pay.Selector;
+            )
+        );
+        require(result, "failed");
+    }
+
+}
