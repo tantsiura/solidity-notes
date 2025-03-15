d9e127558770f949703c42d6f9fe3ade37e19710
Ekaterina <97056496+tantsiura@users.noreply.github.com>
2025-03-15 11:44:30 +0300

    Update create2.sol

--- guide-dao/create2.sol
+++ guide-dao/create2.sol
@@ -6,19 +6,38 @@ contract Demo {
     address public owner;
     event Deployed(address _thisAddr, uint _balance);
 
-    //constructo
+    constructor(address _owner) payable{
+        owner = _owner;
+        emit Deployed(address(this), getBalance()); 
+    }
 
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
 }
 
-contract create2 {
-    event Log(address caller, string calcName, uint result);
+contract Create2 {
+    event Deploy(address _newContract);
 
-    mapping(address => uint) public balances; 
+    function deploy(uint _salt) external payable {
+        Demo newContract = new Demo {
+           salt: bytes32(_salt),
+           value: msg.value
+        }(msg.sender); 
+        emit Deploy(address(newContract));
+    }
 
-    function pay() external payable {
-        balances[msg.sender] += msg.value; 
+    function getByteCode(address _owner) public pure returns(bytes memory) {
+        bytes memory bytecode = type(Demo).creationCode;
+        return abi.encodePacked(bytecode, abi.encode(_owner));
+    }
+
+    function getAddress(bytes memory bytecode, uint _salt) public view returns(address) {
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff), address(this), _salt, keccak256(bytecode)
+            )
+        );
+        return address(uint160(uint(hash)));
     }
 }
